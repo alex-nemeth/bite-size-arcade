57f5d01defe0b597a55499df6ec6821ac4761164
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-15 18:09:50 +0200

    test

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "public");
+
+export default defineConfig({
+    root,
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, "index.html"),
+                sketchbox: resolve(root, "games", "sketchbox.html"),
+                tictactoe: resolve(root, "games", "tictactoe.html"),
+                rockpaperscissors: resolve(
+                    root,
+                    "games",
+                    "rockpaperscissors.html"
+                ),
+            },
+        },
+    },
+});
