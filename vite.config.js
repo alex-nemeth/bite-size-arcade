66adf4227079959bf0cc382fba3cd217ff46172c
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-25 00:54:59 +0200

    react rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
-
+// https://vitejs.dev/config/
 export default defineConfig({
-    root,
-    build: {
-        outDir,
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: resolve(root, "index.html"),
-                sketchbox: resolve(root, "games", "sketchbox.html"),
-                tictactoe: resolve(root, "games", "tictactoe.html"),
-                rockpaperscissors: resolve(
-                    root,
-                    "games",
-                    "rockpaperscissors.html"
-                ),
-            },
-        },
-    },
-});
+  plugins: [react()],
+})
