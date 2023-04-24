fe94a9153db7e15cd8647da01317e651725cbbe5
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-25 01:03:06 +0200

    routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import RockPaperScissors from "./components/rockPaperScissors/RockPaperScissors";
 import Sketchbox from "./components/sketchBox/Sketchbox";
-import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
 
 function App() {
     return (
         <div className="bg-gradient-to-br from-blue-900 to-purple-950 text-white h-screen w-screen">
-            <Navbar />
-            <Sketchbox />
+            <Routes>
+                <Route
+                    path="/sketchbox"
+                    element={
+                        <>
+                            <Navbar /> <Sketchbox />
+                        </>
+                    }
+                />
+            </Routes>
         </div>
     );
 }
