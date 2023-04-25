546b67e725fb882269b74cca7b331759a37a58ec
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-25 11:58:26 +0200

    navbar routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,21 @@ function App() {
     return (
         <div className="bg-gradient-to-br from-blue-900 to-purple-950 text-white h-screen w-screen">
             <Routes>
+                <Route path="/" element={<Navbar />} />
+
                 <Route
                     path="/sketchbox"
                     element={
                         <>
                             <Navbar /> <Sketchbox />
+                        </>
+                    }
+                />
+                <Route
+                    path="/rockpaperscissors"
+                    element={
+                        <>
+                            <Navbar /> <RockPaperScissors />
                         </>
                     }
                 />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
     return (
-        <nav className="flex flex-row justify-between">
-            <div>
-                <a href="/index.html">ByteSizeArcade</a>
-            </div>
+        <nav className="flex flex-row justify-between mb-16">
+            <Link to="/">
+                <div className="flex items-center">
+                    <img
+                        src="/images/logo.png"
+                        alt="ByteSizeArcade Logo"
+                        className="w-14 mr-2"
+                    />
+                    <a href="/" className="font-signature">
+                        ByteSizeArcade
+                    </a>
+                </div>
+            </Link>
             <div className="nav-games">
-                <a>RockPaperScissors</a>
+                <Link to="/sketchbox">
+                    <a>Sketchbox</a>
+                </Link>
+                <Link to="/rockpaperscissors">
+                    <a>RockPaperScissors</a>
+                </Link>
             </div>
         </nav>
     );
