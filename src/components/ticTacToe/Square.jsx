f6d75b326e1cd48d04faff9ef5de08bc36e1465b
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-26 17:14:49 +0200

    ttt winning squares highlight

--- src/components/ticTacToe/Square.jsx
+++ src/components/ticTacToe/Square.jsx
@@ -1,6 +1,11 @@
-export default function Square({ value, onClick }) {
+export default function Square({ value, onClick, winning }) {
     return (
-        <button className="aspect-square border-2 text-3xl" onClick={onClick}>
+        <button
+            className={`aspect-square border-2 text-3xl ${
+                winning && "bg-green-400"
+            }`}
+            onClick={onClick}
+        >
             {value}
         </button>
     );
--- src/components/ticTacToe/TicTacToe.jsx
+++ src/components/ticTacToe/TicTacToe.jsx
@@ -5,6 +5,7 @@ export default function TicTacToe() {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [player, setPlayer] = useState("X");
     const [game, setGame] = useState(true);
+    const [winningSquares, setWinningSquares] = useState([]);
 
     function handleClick(i) {
         if (game) {
@@ -32,19 +33,23 @@ export default function TicTacToe() {
             squares[3] === player &&
             squares[6] === player
         ) {
+            setWinningSquares([0, 3, 6]);
             setGame(false);
         } else if (
             squares[1] === player &&
             squares[4] === player &&
             squares[7] === player
-        )
+        ) {
+            setWinningSquares([1, 4, 7]);
             setGame(false);
-        else if (
+        } else if (
             squares[2] === player &&
             squares[5] === player &&
             squares[8] === player
-        )
+        ) {
+            setWinningSquares([2, 5, 8]);
             setGame(false);
+        }
     }
 
     function checkRows(player) {
@@ -52,20 +57,24 @@ export default function TicTacToe() {
             squares[0] === player &&
             squares[1] === player &&
             squares[2] === player
-        )
+        ) {
+            setWinningSquares([0, 1, 2]);
             setGame(false);
-        else if (
+        } else if (
             squares[3] === player &&
             squares[4] === player &&
             squares[5] === player
-        )
+        ) {
+            setWinningSquares([3, 4, 5]);
             setGame(false);
-        else if (
+        } else if (
             squares[6] === player &&
             squares[7] === player &&
             squares[8] === player
-        )
+        ) {
+            setWinningSquares([6, 7, 8]);
             setGame(false);
+        }
     }
 
     function checkDiagonals() {
@@ -73,37 +82,82 @@ export default function TicTacToe() {
             squares[0] === player &&
             squares[4] === player &&
             squares[8] === player
-        )
+        ) {
+            setWinningSquares([0, 4, 8]);
             setGame(false);
-        else if (
+        } else if (
             squares[2] === player &&
             squares[4] === player &&
             squares[6] === player
-        )
+        ) {
+            setWinningSquares([2, 4, 6]);
             setGame(false);
+        }
     }
 
     function reset() {
         setSquares(Array(9).fill(null));
         setPlayer("X");
         setGame(true);
+        setWinningSquares([]);
     }
 
     return (
         <>
             <div className="grid grid-cols-3 w-10/12 mx-auto">
-                <Square value={squares[0]} onClick={() => handleClick(0)} />
-                <Square value={squares[1]} onClick={() => handleClick(1)} />
-                <Square value={squares[2]} onClick={() => handleClick(2)} />
-                <Square value={squares[3]} onClick={() => handleClick(3)} />
-                <Square value={squares[4]} onClick={() => handleClick(4)} />
-                <Square value={squares[5]} onClick={() => handleClick(5)} />
-                <Square value={squares[6]} onClick={() => handleClick(6)} />
-                <Square value={squares[7]} onClick={() => handleClick(7)} />
-                <Square value={squares[8]} onClick={() => handleClick(8)} />
+                <Square
+                    winning={winningSquares.includes(0) ? true : false}
+                    value={squares[0]}
+                    onClick={() => handleClick(0)}
+                />
+                <Square
+                    winning={winningSquares.includes(1) ? true : false}
+                    value={squares[1]}
+                    onClick={() => handleClick(1)}
+                />
+                <Square
+                    winning={winningSquares.includes(2) ? true : false}
+                    value={squares[2]}
+                    onClick={() => handleClick(2)}
+                />
+                <Square
+                    winning={winningSquares.includes(3) ? true : false}
+                    value={squares[3]}
+                    onClick={() => handleClick(3)}
+                />
+                <Square
+                    winning={winningSquares.includes(4) ? true : false}
+                    value={squares[4]}
+                    onClick={() => handleClick(4)}
+                />
+                <Square
+                    winning={winningSquares.includes(5) ? true : false}
+                    value={squares[5]}
+                    onClick={() => handleClick(5)}
+                />
+                <Square
+                    winning={winningSquares.includes(6) ? true : false}
+                    value={squares[6]}
+                    onClick={() => handleClick(6)}
+                />
+                <Square
+                    winning={winningSquares.includes(7) ? true : false}
+                    value={squares[7]}
+                    onClick={() => handleClick(7)}
+                />
+                <Square
+                    winning={winningSquares.includes(8) ? true : false}
+                    value={squares[8]}
+                    onClick={() => handleClick(8)}
+                />
             </div>
-            <div>
-                <button onClick={reset}>Reset</button>
+            <div className="flex justify-center py-4">
+                <button
+                    className="border-2 rounded-sm px-6 py-4 bg-transparent/50"
+                    onClick={reset}
+                >
+                    Reset
+                </button>
             </div>
         </>
     );
