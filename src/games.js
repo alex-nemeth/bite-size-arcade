22f3da3f162e3cc1fb2e3591f2160413dec7a9df
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-29 19:57:55 +0200

    game descriptions updated

--- src/games.js
+++ src/games.js
@@ -7,25 +7,26 @@ export default [
     },
     {
         title: "Emomemo",
-        description: "testestestes",
+        description: "Can you select all 15 emojis without choosing one twice?",
         img: "/images/thumbnails/emomemo.png",
         url: "/emomemo",
     },
     {
         title: "Sketchbox",
-        description: "testestestes",
+        description:
+            "Draw by hovering over the board like it's an etch-a-sketch!",
         img: "/images/thumbnails/sketchbox.png",
         url: "/sketchbox",
     },
     {
         title: "TicTacToe",
-        description: "testestestes",
+        description: "A timeless classic. Does this need an explanation?",
         img: "/images/thumbnails/tictactoe.png",
         url: "/tictactoe",
     },
     {
         title: "RockPaperScissors",
-        description: "testestestes",
+        description: "Play a set of RPS against the CPU!",
         img: "/images/thumbnails/rockpaperscissors.png",
         url: "/rockpaperscissors",
     },
